9af64f913c07cace2426b1388f09855ea1ab93ad
Antonia Frey <antonia.frey@devoteam.com>
2023-07-30 21:31:19 +0200

    Login user

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    # Check to see if logging in
+    messages.warning(request, "TEST")
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are now logged in!")
+            return redirect('home')
+        else:
+            messages.warning(request, "An error occured, please try again!")
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    pass
+
+
+"""
+def login_user(request):
+    pass
+"""
